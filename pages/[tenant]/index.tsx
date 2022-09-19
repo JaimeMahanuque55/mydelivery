48c44179686983a63385795a60f0ede31572850a
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2022-09-19 13:24:39 +0100

    Tela cardápio 2

--- pages/[tenant]/index.tsx
+++ pages/[tenant]/index.tsx
@@ -2,8 +2,21 @@ import styles from '../../styles/Home.module.css';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Seja bem vindo</h1>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.headerTop}>
+          <div className={styles.headerTopLeft}>
+            <div className={styles.headerTitle}>Seja Bem Vindo(a) 👋</div>
+            <div className={styles.headerSubtitle}>O que deseja para hoje?</div>
+          </div>
+          <div className={styles.headerTopRight}>
+
+          </div>
+        </div>
+        <div className={styles.headerBottom}>
+          Busca
+        </div>
+      </header>
     </div>
   );
 }
