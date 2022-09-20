8bbb0f7772693ec57ca82c837129c89dba7dc41c
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2022-09-20 19:08:30 +0100

    Tela cardápio: Banner Promoções 3

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,7 +1,8 @@
 import styles from './styles.module.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper';
 import 'swiper/css';
-// import './swiper.css'
+
 
 
 export const Banner = () => {
@@ -9,7 +10,13 @@ export const Banner = () => {
     <div className={styles.container}>
       <Swiper
         slidesPerView={1}
+        autoplay={{
+          delay: 1000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
         className={styles.swiper}
+        modules={[Autoplay]}
       >
         <SwiperSlide className={styles.slide}><div className={styles.slideImg}>1</div></SwiperSlide>
         <SwiperSlide className={styles.slide}><div className={styles.slideImg}>2</div></SwiperSlide>
