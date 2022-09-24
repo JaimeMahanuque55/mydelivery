de3ef8e9101a6f164fafc0e7f95f10b4af9cf269
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2022-09-24 17:18:07 +0100

    Tela cardápio: Cards Produtos 2

--- components/ProductItem/index.tsx
+++ components/ProductItem/index.tsx
@@ -2,6 +2,16 @@ import styles from './styles.module.css';
 
 export const ProductItem = () => {
   return (
-    <div className={styles.container}>...</div>
+    <div className={styles.container}>
+      <div className={styles.head}></div>
+      <div className={styles.info}>
+        <div className={styles.img}>
+          <img src="/temp/burger.png" alt="" />
+        </div>
+        <div className={styles.catName}>Tradicional</div>
+        <div className={styles.name}>Texas Burger</div>
+        <div className={styles.price}>MZN 120</div>
+      </div>
+    </div>
   )
 }
