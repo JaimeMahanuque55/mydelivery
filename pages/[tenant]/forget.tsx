bfc18bbafea763351c79e0dce8699be4a4e9d060
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2022-10-14 23:33:21 +0100

    Pagina esqueci a senha

---
+++ pages/[tenant]/forget.tsx
@@ -0,0 +1,97 @@
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { InpuField } from '../../components/InputField';
+import { useAppContext } from '../../contexts/AppContext';
+import { useApi } from '../../libs/useApi';
+import styles from '../../styles/forget.module.css';
+import { Tenant } from '../../types/Tenant';
+
+const Forget = (data: Props) => {
+  const { tenant, setTenant } = useAppContext();
+
+  useEffect(() => {
+    setTenant(data.tenant);
+  }, []);
+
+  const router = useRouter();
+
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = () => {
+    router.push(`${data.tenant.slug}/forget-success`);
+  }
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Esqueci a senha | {data.tenant.name}</title>
+      </Head>
+
+      <Header
+        color={data.tenant.mainColor}
+        backHref={`/${data.tenant.slug}/login`}
+      />
+
+      <div className={styles.header}>{data.tenant.name}</div>
+      <div className={styles.title}>Esqueceu sua senha?</div>
+      <div
+        className={styles.subtitle}
+        style={{ borderBottomColor: data.tenant.mainColor }}
+      >
+        Preencha o campo com seu e-mail e receba as instruções necessárias para redefinir a sua senha.
+      </div>
+      <div className={styles.line}></div>
+
+      <div className={styles.formArea}>
+        <div className={styles.inputArea}>
+          <InpuField
+            color={data.tenant.mainColor}
+            placeholder='Digite seu e-mail'
+            value={email}
+            onChange={setEmail}
+          />
+        </div>
+        <div className={styles.inputArea}>
+          <Button
+            color={data.tenant.mainColor}
+            label="Enviar"
+            onClick={handleSubmit}
+            fill={true}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Forget;
+
+type Props = {
+  tenant: Tenant
+}
+
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { tenant: tenantSlug } = context.query;
+  const api = useApi();
+
+  // Get Tenant
+  const tenant = await api.getTenant(tenantSlug as string);
+  if (!tenant) {
+    return {
+      redirect: { destination: '/', permanent: false }
+    }
+  }
+
+  return {
+    props: {
+      tenant
+    }
+  }
+
+}
